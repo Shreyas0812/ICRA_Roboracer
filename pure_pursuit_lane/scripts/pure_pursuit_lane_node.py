4f88cd26e1ae9db59da8003cde2fef0ef9924788
Shreyas Raorane <raorane@seas.upenn.edu>
2025-05-18 14:14:29 -0400

    config part added

--- pure_pursuit_lane/scripts/pure_pursuit_lane_node.py
+++ pure_pursuit_lane/scripts/pure_pursuit_lane_node.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
+import os
+
 import rclpy
 from rclpy.node import Node
+from rclpy.qos import QoSProfile, ReliabilityPolicy, DurabilityPolicy
+
+from nav_msgs.msg import Odometry
+
+from transforms3d.euler import quat2euler
+import numpy as np
+
+from ament_index_python.packages import get_package_share_directory
 
 class PurePursuitLaneNode(Node):
     def __init__(self):
